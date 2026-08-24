75e1a75b432ab4e34c3d5f96a9b003098e83f36f
Rupam Mazumder <utpalmajumdar6@gmail.com>
2026-08-24 13:11:37 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (83.43%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -4,7 +4,7 @@ public:
     bool rotateString(string s, string goal) {
         if (s.length() != goal.length()) return false;
         
-        // Concatenate s with itself and check if goal exists in it
+        
         string doubledS = s + s;
         return doubledS.find(goal) != string::npos;
     }

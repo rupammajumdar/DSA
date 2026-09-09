33db0866c8f2935c83a246053d41c6e2a8e008a0
Rupam Mazumder <utpalmajumdar6@gmail.com>
2026-09-09 13:05:12 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (32.10%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -5,33 +5,25 @@ public:
         if (!head || !head->next || k == 0) {
             return head;
         }
+        ListNode* curr =head;
 
-        // 1. Find the length of the list and the original tail node
-        ListNode* curr = head;
-        int len = 1;
-        while (curr->next != NULL) {
-            len++;
-            curr = curr->next;
-        }
+      int cnt=1;
+      while(curr->next!=NULL){
+        cnt++;
+        curr=curr->next;
 
-        // 2. Connect the tail to the head to form a circular linked list
-        curr->next = head;
+      }
+      curr->next=head;
+      k= k%cnt;
+      int newstep=cnt-k;
+      ListNode* temp=curr->next;
+      while(newstep!=1){
+        temp=temp->next;
+        newstep--;
+      }
+      ListNode* NewHead = temp->next;
+      temp->next=NULL;
+      return NewHead;
 
-        // 3. Calculate effective rotations
-        k = k % len;
-        
-        // 4. Find the new tail, which is (len - k) steps from the current head
-        int stepsToNewTail = len - k;
-        ListNode* newTail = curr; // We can continue from the current tail
-        while (stepsToNewTail > 0) {
-            newTail = newTail->next;
-            stepsToNewTail--;
-        }
-
-        // 5. Break the circle to establish the new head and tail
-        ListNode* newHead = newTail->next;
-        newTail->next = NULL;
-
-        return newHead;
     }
 };
